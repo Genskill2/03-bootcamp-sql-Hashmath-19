90c86f80f019e7225e49f0b1c9c4713ef03b9b35
Hashmath-19 <84575247+Hashmath-19@users.noreply.github.com>
2021-06-19 18:09:33 +0530

    Delete query3.sql

--- query3.sql
+++
@@ -1,4 +0,0 @@
-SELECT b.title,p.publisher from books b, publishers p 
-                where title IN(SELECT book from book_subjects 
-                where subject="Technology" OR subject="Politics");
-                
